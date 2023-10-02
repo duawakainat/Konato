0be5133fdc0acfb2963a4b5600e14e2a7e207234
duawakainat <you@example.com>
2023-10-02 01:17:04 -0700

    completed

---
+++ asset/bootstrap/js/app.js
@@ -0,0 +1,122 @@
+
+$(window).resize(function () {
+    if ($(window).width() <= 998) {
+        $('.navbar-toggler').hide();
+
+    }
+});
+
+
+$(".BTN_TOP").click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, "slow");
+    return false;
+})
+
+
+// $(window).on('resize', function () {
+//     if ($(window).width() <= 998) {
+//         console.log('yes');
+//         let AddValue = $(".MainTop").html();
+//         // AddValue.css('color', 'black');
+//         $(".addNavMenu").html(AddValue);
+//         $(".addNavMenu").css("display", "block")
+//         $('.nav-link').css("list-style-type", "none")
+//         $('.toggle').toggle()
+//     } else {
+//         $(".addNavMenu").html('');
+//     }
+// });
+
+
+$(".nav-show-hiden").css("display", "none");
+$(window).resize(function () {
+    if ($(window).width() <= 998) {
+        $('.nav-show-hiden').show();
+
+    }
+});
+$(window).resize(function () {
+    if ($(window).width() >= 998) {
+        $('.nav-show-hiden').hide();
+
+    }
+});
+$(window).on('resize', function () {
+    if ($(window).width() >= 578) {
+        $('.Map_Hidden').show();
+        console.log('helo i not');
+    }
+});
+
+$(window).on('resize', function () {
+    if ($(window).width() <= 578) {
+        $('.Map_Hidden').hide();
+        console.log('helo i maam');
+    }
+});
+// $('#formGroupExampleInput').addClass('fa-solid fa-bed');
+
+
+$('.owl-first-one').owlCarousel({
+    loop: true,
+    margin: 10,
+    dotted: true,
+    responsive: {
+        300: {
+            items: 1,
+        },
+        778: {
+            items: 2,
+        },
+        998: {
+            items: 2,
+        },
+        1100: {
+            items: 3,
+        }
+    }
+})
+
+$('.owl-first').owlCarousel({
+    loop: true,
+    margin: 10,
+    dotted: true,
+    responsive: {
+        300: {
+            items: 1,
+        },
+        778: {
+            items: 2,
+        },
+        998: {
+            items: 2,
+        },
+        1100: {
+            items: 3,
+        }
+    }
+})
+$('.owl-second').owlCarousel({
+    loop: true,
+    margin: 10,
+    dotted: true,
+    responsive: {
+        300: {
+            items: 1,
+        },
+        578: {
+            items: 2,
+        },
+        778: {
+            items: 3,
+        },
+        998: {
+            items: 4,
+        },
+        1100: {
+            items: 4,
+        }
+    }
+})
